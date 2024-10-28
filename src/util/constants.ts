50d1bd31ad614584b8ade38fff21309a30c38c88
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-28 16:07:45 -0400

    Add basic constants

---
+++ src/util/constants.ts
@@ -0,0 +1,13 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 28 Oct 2024, 3:53:18 PM
+ *  Last update: 28 Oct 2024, 4:07:12 PM
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
+import { Platform } from "react-native"
+
+export const primaryColor = "#2A324B";
+export const accentColor = "#E6AF2E";
+export const white = "#FBFFFE";
+
+export const safeAreaPadding = Platform.OS === "ios" ? 60 : 50;

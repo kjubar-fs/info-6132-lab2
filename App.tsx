e1f6e3acf90ac4a35a5fd5d902fc52ff37fa7c36
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-28 16:08:07 -0400

    Add view wrapper for home screen

--- App.tsx
+++ App.tsx
@@ -1,19 +1,34 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Oct 1985, 4:15:00 AM
- *  Last update: 28 Oct 2024, 3:49:04 PM
+ *  Last update: 28 Oct 2024, 4:07:32 PM
  *  Copyright (c) 1985 - 2024 Kaleb Jubar
  */
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, View } from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
+
+import { NavigationContainer } from '@react-navigation/native';
+
 import HomeScreen from './src/screens/HomeScreen';
+
+import { primaryColor } from './src/util/constants';
 
 export default function App() {
     return (
         <NavigationContainer>
-            <StatusBar style="auto" />
+            <StatusBar style="light" />
 
-            <HomeScreen />
+            <View style={styles.container}>
+                <HomeScreen />
+            </View>
         </NavigationContainer>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: primaryColor,
+    },
+});

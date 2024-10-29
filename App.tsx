1c69f2881178b3963b99338555415ed2ba60f061
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-29 14:41:06 -0400

    Add transaction list mock data and provider

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Oct 1985, 4:15:00 AM
- *  Last update: 28 Oct 2024, 4:07:32 PM
+ *  Last update: 29 Oct 2024, 2:00:48 PM
  *  Copyright (c) 1985 - 2024 Kaleb Jubar
  */
 import { StyleSheet, View } from 'react-native';
@@ -10,6 +10,9 @@ import { StatusBar } from 'expo-status-bar';
 
 import { NavigationContainer } from '@react-navigation/native';
 
+import { TransactionsProvider } from './src/data/context/transactions';
+import transactions from "./src/data/mock/transactions.json";
+
 import HomeScreen from './src/screens/HomeScreen';
 
 import { primaryColor } from './src/util/constants';
@@ -17,11 +20,13 @@ import { primaryColor } from './src/util/constants';
 export default function App() {
     return (
         <NavigationContainer>
+            <TransactionsProvider txList={transactions}>
             <StatusBar style="light" />
 
             <View style={styles.container}>
                 <HomeScreen />
             </View>
+            </TransactionsProvider>
         </NavigationContainer>
     );
 }

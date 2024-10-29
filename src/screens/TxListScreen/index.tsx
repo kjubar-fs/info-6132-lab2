b442a164ad15283f42f4bcbcdffafa9529f94879
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-29 15:58:21 -0400

    Implement transactions list

--- src/screens/TxListScreen/index.tsx
+++ src/screens/TxListScreen/index.tsx
@@ -1,15 +1,20 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 28 Oct 2024, 3:50:14 PM
- *  Last update: 
+ *  Last update: 29 Oct 2024, 3:41:09 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import { Text, View } from "react-native";
+import { View, Text } from "react-native";
+
+import TransactionList from "../../components/transactions/TransactionList";
+
+import styles from "./styles";
 
 export default function TxListScreen() {
     return (
-        <View>
-            <Text>Transaction List screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Transactions</Text>
+            <TransactionList />
         </View>
     );
 }
---
+++ src/screens/TxListScreen/styles.ts
@@ -0,0 +1,23 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 29 Oct 2024, 10:28:32 AM
+ *  Last update: 29 Oct 2024, 3:50:45 PM
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        alignItems: "center",
+        padding: 20,
+        paddingBottom: 110,
+        gap: 20,
+    },
+
+    title: {
+        alignSelf: "flex-start",
+        
+        fontSize: 20,
+        fontWeight: "700",
+    },
+});

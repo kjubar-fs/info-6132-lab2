c84b2410e1a7f69d8b42591bafab5013f958a704
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-29 15:36:48 -0400

    Implement summary screen

--- src/screens/SummaryScreen/index.tsx
+++ src/screens/SummaryScreen/index.tsx
@@ -1,15 +1,45 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 28 Oct 2024, 3:50:02 PM
- *  Last update: 
+ *  Last update: 29 Oct 2024, 3:34:36 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import { Text, View } from "react-native";
+import { Text, ScrollView, TouchableHighlight } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+
+import Card from "../../components/common/Card";
+import Divider from "../../components/common/Divider";
+import BalanceListing from "../../components/summary/BalanceListing";
+import BalanceHistory from "../../components/summary/BalanceHistory";
+import TransactionList from "../../components/transactions/TransactionList";
+
+import { navScreens } from "../../util/constants";
+import styles from "./styles";
 
 export default function SummaryScreen() {
+    const nav = useNavigation();
+
     return (
-        <View>
-            <Text>Summary screen</Text>
-        </View>
+        <ScrollView contentContainerStyle={styles.container}>
+            <Card shadowType="light">
+                <Text style={styles.title}>Current Balance</Text>
+                <BalanceListing balance={50000} dayChange={10} />
+                <Divider />
+                <BalanceHistory />
+            </Card>
+            <Card>
+                <Text style={styles.title}>Recent Transactions</Text>
+                <TransactionList flatDisplay={true} />
+                <Divider />
+                <TouchableHighlight
+                    style={styles.button}
+                    underlayColor="#F7F7F7"
+                    onPress={() => { nav?.navigate(navScreens.transactions as never) }}
+                >
+                    <Text>View More</Text>
+                </TouchableHighlight>
+            </Card>
+        </ScrollView>
     );
 }
---
+++ src/screens/SummaryScreen/styles.ts
@@ -0,0 +1,31 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 29 Oct 2024, 10:28:32 AM
+ *  Last update: 29 Oct 2024, 3:10:21 PM
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        alignItems: "center",
+        padding: 20,
+        gap: 20,
+    },
+
+    title: {
+        alignSelf: "flex-start",
+        paddingBottom: 10,
+        
+        fontSize: 20,
+        fontWeight: "700",
+    },
+
+    button: {
+        minWidth: "100%",
+        alignItems: "center",
+        padding: 2,
+
+        borderRadius: 3,
+    },
+});

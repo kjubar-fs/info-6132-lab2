8dfa46251418d90ce1bdeffb3dcacc1644e2d430
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-29 12:48:58 -0400

    Reorganize component hierarchy

--- src/components/common/Card/styles.ts
+++ src/components/common/Card/styles.ts
@@ -1,12 +1,12 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 Oct 2024, 10:20:36 AM
- *  Last update: 29 Oct 2024, 10:37:03 AM
+ *  Last update: 29 Oct 2024, 12:46:58 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { StyleSheet } from "react-native";
 
-import { dropShadowStyle, lightDropShadowStyle } from "../../util/constants";
+import { dropShadowStyle, lightDropShadowStyle } from "../../../util/constants";
 
 export default StyleSheet.create({
     container: {

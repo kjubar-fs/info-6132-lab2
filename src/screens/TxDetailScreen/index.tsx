ec29ad85e816b3271bda2b123a2cfaaca6e01b01
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-30 12:21:13 -0400

    Add transaction details

--- src/screens/TxDetailScreen/index.tsx
+++ src/screens/TxDetailScreen/index.tsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 Oct 2024, 4:05:16 PM
- *  Last update: 30 Oct 2024, 11:51:05 AM
+ *  Last update: 30 Oct 2024, 12:16:27 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { View, Text } from "react-native";
@@ -11,6 +11,7 @@ import { useRoute } from "@react-navigation/native";
 import { isTransaction, Transaction } from "../../data/context/transactions";
 
 import styles from "./styles";
+import { formatDate, negativeColor, numFormatter, positiveColor } from "../../util/constants";
 
 export default function TxDetailScreen() {
     const route = useRoute();
@@ -19,12 +20,42 @@ export default function TxDetailScreen() {
             <View style={styles.error}>
                 <Text style={styles.errorText}>No transaction found!</Text>
             </View>
-        )
+        );
     }
 
+    const tx = route.params as Transaction;
+
     return (
-        <View>
-            <Text>Transaction details</Text>
+        <View style={styles.container}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>{tx.name}</Text>
+                <Text style={styles.subtitle}>{formatDate(new Date(tx.date))}</Text>
+            </View>
+
+            <View>
+                <Text
+                    style={[
+                        styles.amountText,
+                        { color: tx.value > 0 ? positiveColor : negativeColor },
+                    ]}
+                >{numFormatter.format(tx.value)}</Text>
+            </View>
+
+            <View style={styles.keyValueContainer}>
+                <Text style={[styles.kvText, styles.key]}>Sender:</Text>
+                <Text style={styles.kvText}>{tx.sender}</Text>
+            </View>
+
+            <View style={styles.keyValueContainer}>
+                <Text style={[styles.kvText, styles.key]}>Destination:</Text>
+                <Text style={styles.kvText}>{tx.destination}</Text>
+            </View>
+
+            {tx.description !== undefined &&
+            <View style={styles.titleContainer}>
+                <Text style={[styles.kvText, styles.key]}>Additional Information:</Text>
+                <Text style={styles.kvText}>{tx.description}</Text>
+            </View>}
         </View>
     );
 }
--- src/screens/TxDetailScreen/styles.ts
+++ src/screens/TxDetailScreen/styles.ts
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 Oct 2024, 4:05:26 PM
- *  Last update: 30 Oct 2024, 11:32:16 AM
+ *  Last update: 30 Oct 2024, 12:14:50 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { StyleSheet } from "react-native";
@@ -15,5 +15,43 @@ export default StyleSheet.create({
 
     errorText: {
         fontSize: 18,
+    },
+
+    container: {
+        gap: 15,
+        padding: 30,
+    },
+
+    titleContainer: {
+        gap: 5,
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: "600",
+    },
+
+    subtitle: {
+        fontSize: 15,
+        fontWeight: "300",
+        color: "#777",
+    },
+
+    amountText: {
+        fontSize: 20,
+        fontWeight: "500",
+    },
+
+    keyValueContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+
+    kvText: {
+        fontSize: 16,
+    },
+
+    key: {
+        fontWeight: "600",
     },
 });

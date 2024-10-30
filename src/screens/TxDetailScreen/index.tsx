b3505f0577ecf0f82802536584214b3792be449f
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-30 11:35:57 -0400

    Add tx not found error

--- src/screens/TxDetailScreen/index.tsx
+++ src/screens/TxDetailScreen/index.tsx
@@ -1,12 +1,26 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 Oct 2024, 4:05:16 PM
- *  Last update: 
+ *  Last update: 30 Oct 2024, 11:33:14 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { View, Text } from "react-native";
 
+import { useRoute } from "@react-navigation/native";
+
+import styles from "./styles";
+
 export default function TxDetailScreen() {
+    const route = useRoute();
+    console.log(route);
+    if (!route || !route.params) {
+        return (
+            <View style={styles.error}>
+                <Text style={styles.errorText}>No transaction found!</Text>
+            </View>
+        )
+    }
+
     return (
         <View>
             <Text>Transaction details</Text>
--- src/screens/TxDetailScreen/styles.ts
+++ src/screens/TxDetailScreen/styles.ts
@@ -1,11 +1,19 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 Oct 2024, 4:05:26 PM
- *  Last update: 
+ *  Last update: 30 Oct 2024, 11:32:16 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+    error: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
 
+    errorText: {
+        fontSize: 18,
+    },
 });

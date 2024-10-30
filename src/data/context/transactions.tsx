e6d6591f420106e10cd6a792222cea12dd08f0db
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-30 11:51:56 -0400

    Add transaction type checking function

--- src/data/context/transactions.tsx
+++ src/data/context/transactions.tsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 Oct 2024, 1:51:38 PM
- *  Last update: 29 Oct 2024, 2:13:00 PM
+ *  Last update: 30 Oct 2024, 11:49:49 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { createContext, useContext } from "react";
@@ -20,6 +20,17 @@ export type Transaction = {
     sender: string,
     destination: string,
     description?: string
+}
+
+export function isTransaction(tx: Transaction): tx is Transaction {
+    const txCast = tx as Transaction;
+    return (
+        txCast.value !== undefined &&
+        txCast.date !== undefined &&
+        txCast.name !== undefined &&
+        txCast.sender !== undefined &&
+        txCast.destination !== undefined
+    );
 }
 
 export function TransactionsProvider({ children, txList }: TransactionsProviderProps) {
--- src/screens/TxDetailScreen/index.tsx
+++ src/screens/TxDetailScreen/index.tsx
@@ -1,19 +1,20 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 Oct 2024, 4:05:16 PM
- *  Last update: 30 Oct 2024, 11:33:14 AM
+ *  Last update: 30 Oct 2024, 11:51:05 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { View, Text } from "react-native";
 
 import { useRoute } from "@react-navigation/native";
 
+import { isTransaction, Transaction } from "../../data/context/transactions";
+
 import styles from "./styles";
 
 export default function TxDetailScreen() {
     const route = useRoute();
-    console.log(route);
-    if (!route || !route.params) {
+    if (!route || !route.params || !isTransaction(route.params as Transaction)) {
         return (
             <View style={styles.error}>
                 <Text style={styles.errorText}>No transaction found!</Text>
